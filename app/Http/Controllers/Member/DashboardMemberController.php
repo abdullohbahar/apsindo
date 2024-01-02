b1927ace8dfdb2712a14e4deb1ed303cf3f15cec
Baharudin Abdulloh Mun'im <abdullohbahar@gmail.com>
2024-01-02 11:48:40 +0700

    hide payment button on dashboard member when already paid

--- app/Http/Controllers/Member/DashboardMemberController.php
+++ app/Http/Controllers/Member/DashboardMemberController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Member;
 
 use App\Http\Controllers\Controller;
+use App\Models\Subscription;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -10,8 +11,13 @@ class DashboardMemberController extends Controller
 {
     public function index()
     {
+        $subscribe = Subscription::where('user_id', auth()->user()->id)
+            ->orderBy('created_at', 'desc')
+            ->first();
+
         $data = [
-            'active' => 'dashboard'
+            'active' => 'dashboard',
+            'subscribe' => $subscribe
         ];
 
         return view('member.dashboard.index', $data);
